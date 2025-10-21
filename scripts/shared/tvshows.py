9d59ed4890b3352d2a17261e4a8154c6204a820c
Felix Rizzolli <felix.rizzolli@outlook.de>
2025-10-21 15:21:44 +0000

    feat: add tvshows.py with a list of tvshows

---
+++ scripts/shared/tvshows.py
@@ -0,0 +1,133 @@
+"""Shared TV show configuration for test library generators."""
+
+# Consolidated TV show data with episode counts
+tvshows: list[dict[str, object]] = [
+    {
+        "name": "Game of Thrones (2011) {tvdb-121361}",
+        "episodes": {1: 10, 2: 10, 3: 10, 4: 10, 5: 10, 6: 10, 7: 7, 8: 6},
+    },
+    {
+        "name": "House of the Dragon (2022) {tvdb-121361}",
+        "episodes": {1: 10, 2: 8},
+    },
+    {
+        "name": "The Lord of the Rings - The Rings of Power (2022) {tvdb-367506}",
+        "episodes": {1: 8, 2: 8},
+    },
+    {
+        "name": "The Witcher (2019) {tvdb-362696}",
+        "episodes": {1: 8, 2: 8, 3: 8, 4: 8},
+    },
+    {
+        "name": "The Witcher - Blood Origin (2022) {tvdb-399987}",
+        "episodes": {1: 4},
+    },
+    {
+        "name": "Alien - Earth (2025) {tvdb-458912}",
+        "episodes": {1: 8},
+    },
+    {
+        "name": "Fallout (2024) {tvdb-416744}",
+        "episodes": {1: 8, 2: 8},
+    },
+    {
+        "name": "The Recruit (2022) {tvdb-425260}",
+        "episodes": {1: 8, 2: 6},
+    },
+    {
+        "name": "The Night Agent (2023) {tvdb-407281}",
+        "episodes": {1: 10, 2: 10},
+    },
+    {
+        "name": "Alex Rider (2020) {tvdb-370312}",
+        "episodes": {1: 8, 2: 8, 3: 8},
+    },
+    {
+        "name": "Hanna (2019) {tvdb-357690}",
+        "episodes": {1: 8, 2: 8, 3: 6},
+    },
+    {
+        "name": "Wednesday (2022) {tvdb-397060}",
+        "episodes": {1: 8, 2: 8},
+    },
+    {
+        "name": "Reacher (2022) {tvdb-366924}",
+        "episodes": {1: 8, 2: 8, 3: 8},
+    },
+    {
+        "name": "Warrior Nun (2020) {tvdb-366924}",
+        "episodes": {1: 10, 2: 8},
+    },
+    {
+        "name": "Lupin (2021) {tvdb-375921}",
+        "episodes": {1: 10, 2: 7},
+    },
+    {
+        "name": "The Terminal List (2022) {tvdb-399917}",
+        "episodes": {1: 8},
+    },
+    {
+        "name": "The Terminal List - Dark Wolf (2025) {tvdb-464185}",
+        "episodes": {1: 7},
+    },
+    {
+        "name": "3 Body Problem (2024) {tvdb-411959}",
+        "episodes": {1: 8},
+    },
+    {
+        "name": "Prison Break (2005) {tvdb-121361}",
+        "episodes": {1: 22, 2: 22, 3: 13, 4: 22, 5: 9},
+    },
+    {
+        "name": "Breaking Bad (2008) {tvdb-81189}",
+        "episodes": {1: 7, 2: 13, 3: 13, 4: 13, 5: 16},
+    },
+    {
+        "name": "Better Call Saul (2015) {tvdb-273181}",
+        "episodes": {1: 10, 2: 10, 3: 10, 4: 10, 5: 10, 6: 13},
+    },
+    {
+        "name": "Orphan Black (2013) {tvdb-260315}",
+        "episodes": {1: 10, 2: 10, 3: 10, 4: 10, 5: 10},
+    },
+    {
+        "name": "The 100 (2014) {tvdb-268592}",
+        "episodes": {1: 13, 2: 16, 3: 16, 4: 13, 5: 13, 6: 13, 7: 16},
+    },
+    {
+        "name": "Attack on Titan (2013) {tvdb-267440}",
+        "episodes": {1: 25, 2: 12, 3: 22, 4: 28},
+    },
+    {
+        "name": "One Punch Man (2015) {tvdb-293088}",
+        "episodes": {1: 12, 2: 12, 3: 12},
+    },
+    {
+        "name": "Overlord (2015) {tvdb-294002}",
+        "episodes": {1: 13, 2: 13, 3: 13, 4: 13},
+    },
+    {
+        "name": "Code Geass (2006) {tvdb-79525}",
+        "episodes": {1: 25, 2: 25},
+    },
+    {
+        "name": "Death Note (2006) {tvdb-79481}",
+        "episodes": {1: 37},
+    },
+    {
+        "name": "Classroom of the Elite (2017) {tvdb-329822}",
+        "episodes": {1: 12, 2: 13, 3: 13},
+    },
+    {
+        "name": "My Name (2021) {tvdb-397441}",
+        "episodes": {1: 8},
+    },
+    {
+        "name": "Squid Game (2021) {tvdb-383275}",
+        "episodes": {1: 9, 2: 7, 3: 6},
+    },
+    {
+        "name": "The Day of the Jackal (2024) {tvdb-426866}",
+        "episodes": {1: 10},
+    },
+]
