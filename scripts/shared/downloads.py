d34fb32b6a584ea4f55fea48e131ea75ff94f2ae
Felix Rizzolli <felix.rizzolli@outlook.de>
2025-10-21 14:57:22 +0000

    refactor: format downloads.py

--- scripts/shared/downloads.py
+++ scripts/shared/downloads.py
@@ -1,128 +1,164 @@
 downloads = [
-    # AVI
+    # ----------------------------------------------------------------------------------------------------------########
+    # ----------------------------------------------------------------------------------------------------------# AVI  #
+    # ----------------------------------------------------------------------------------------------------------########
     {
         "size": "750KB",
         "format": "avi",
         "resolution": "640x480",
-        "url": "https://file-examples.com/wp-content/storage/2018/04/file_example_AVI_480_750kB.avi"},
+        "url": "https://file-examples.com/wp-content/storage/2018/04/file_example_AVI_480_750kB.avi"
+    },
     {
         "size": "800KB",
         "format": "avi",
         "resolution": "640x480",
-        "url": "https://file-examples.com/wp-content/storage/2018/04/file_example_AVI_640_800kB.avi"},
+        "url": "https://file-examples.com/wp-content/storage/2018/04/file_example_AVI_640_800kB.avi"
+    },
     {
         "size": "1.5MB",
         "format": "avi",
         "resolution": "1280x720",
-        "url": "https://file-examples.com/wp-content/storage/2018/04/file_example_AVI_1280_1_5MG.avi"},
+        "url": "https://file-examples.com/wp-content/storage/2018/04/file_example_AVI_1280_1_5MG.avi"
+    },
     {
         "size": "2.3MB",
         "format": "avi",
         "resolution": "1920x1080",
-        "url": "https://file-examples.com/wp-content/storage/2018/04/file_example_AVI_1920_2_3MG.avi"},
-    # MOV
+        "url": "https://file-examples.com/wp-content/storage/2018/04/file_example_AVI_1920_2_3MG.avi"
+    },
+    # ----------------------------------------------------------------------------------------------------------########
+    # ----------------------------------------------------------------------------------------------------------# MOV  #
+    # ----------------------------------------------------------------------------------------------------------########
     {
         "size": "700KB",
         "format": "mov",
         "resolution": "640x480",
-        "url": "https://file-examples.com/wp-content/storage/2018/04/file_example_MOV_480_700kB.mov"},
+        "url": "https://file-examples.com/wp-content/storage/2018/04/file_example_MOV_480_700kB.mov"
+    },
     {
         "size": "800KB",
         "format": "mov",
         "resolution": "640x480",
-        "url": "https://file-examples.com/wp-content/storage/2018/04/file_example_MOV_640_800kB.mov"},
+        "url": "https://file-examples.com/wp-content/storage/2018/04/file_example_MOV_640_800kB.mov"
+    },
     {
         "size": "1.4MB",
         "format": "mov",
         "resolution": "1280x720",
-        "url": "https://file-examples.com/wp-content/storage/2018/04/file_example_MOV_1280_1_4MB.mov"},
+        "url": "https://file-examples.com/wp-content/storage/2018/04/file_example_MOV_1280_1_4MB.mov"
+    },
     {
         "size": "2.2MB",
         "format": "mov",
         "resolution": "1920x1080",
-        "url": "https://file-examples.com/wp-content/storage/2018/04/file_example_MOV_1920_2_2MB.mov"},
-    # MP4
+        "url": "https://file-examples.com/wp-content/storage/2018/04/file_example_MOV_1920_2_2MB.mov"
+    },
+    # ----------------------------------------------------------------------------------------------------------########
+    # ----------------------------------------------------------------------------------------------------------# MP4  #
+    # ----------------------------------------------------------------------------------------------------------########
     {
         "size": "1.5MB",
         "format": "mp4",
         "resolution": "640x480",
-        "url": "https://file-examples.com/storage/fe77ccaeb468f77bd967b4f/2017/04/file_example_MP4_480_1_5MG.mp4"},
+        "url": "https://file-examples.com/storage/fe77ccaeb468f77bd967b4f/2017/04/file_example_MP4_480_1_5MG.mp4"
+    },
     {
         "size": "3MB",
         "format": "mp4",
         "resolution": "640x360",
-        "url": "https://file-examples.com/storage/fe77ccaeb468f77bd967b4f/2017/04/file_example_MP4_640_3MG.mp4"},
+        "url": "https://file-examples.com/storage/fe77ccaeb468f77bd967b4f/2017/04/file_example_MP4_640_3MG.mp4"
+    },
     {
         "size": "10MB",
         "format": "mp4",
         "resolution": "1280x720",
-        "url": "https://file-examples.com/storage/fe77ccaeb468f77bd967b4f/2017/04/file_example_MP4_1280_10MG.mp4"},
+        "url": "https://file-examples.com/storage/fe77ccaeb468f77bd967b4f/2017/04/file_example_MP4_1280_10MG.mp4"
+    },
     {
         "size": "18MB",
         "format": "mp4",
         "resolution": "1920x1080",
-        "url": "https://file-examples.com/storage/fe77ccaeb468f77bd967b4f/2017/04/file_example_MP4_1920_18MG.mp4"},
-    # OGG
+        "url": "https://file-examples.com/storage/fe77ccaeb468f77bd967b4f/2017/04/file_example_MP4_1920_18MG.mp4"
+    },
+    # ----------------------------------------------------------------------------------------------------------########
+    # ----------------------------------------------------------------------------------------------------------# OGG  #
+    # ----------------------------------------------------------------------------------------------------------########
     {
         "size": "1.7MB",
         "format": "ogg",
         "resolution": "640x480",
-        "url": "https://file-examples.com/storage/fe77ccaeb468f77bd967b4f/2018/04/file_example_OGG_480_1_7mg.ogg"},
+        "url": "https://file-examples.com/storage/fe77ccaeb468f77bd967b4f/2018/04/file_example_OGG_480_1_7mg.ogg"
+    },
     {
         "size": "2.7MB",
         "format": "ogg",
         "resolution": "640x360",
-        "url": "https://file-examples.com/storage/fe77ccaeb468f77bd967b4f/2018/04/file_example_OGG_640_2_7mg.ogg"},
+        "url": "https://file-examples.com/storage/fe77ccaeb468f77bd967b4f/2018/04/file_example_OGG_640_2_7mg.ogg"
+    },
     {
         "size": "11.4MB",
         "format": "ogg",
         "resolution": "1280x720",
-        "url": "https://file-examples.com/storage/fe77ccaeb468f77bd967b4f/2018/04/file_example_OGG_1280_11_4mg.ogg"},
+        "url": "https://file-examples.com/storage/fe77ccaeb468f77bd967b4f/2018/04/file_example_OGG_1280_11_4mg.ogg"
+    },
     {
         "size": "13.3MB",
         "format": "ogg",
         "resolution": "1920x1080",
-        "url": "https://file-examples.com/storage/fe77ccaeb468f77bd967b4f/2018/04/file_example_OGG_1920_13_3mg.ogg"},
-    # WMV
+        "url": "https://file-examples.com/storage/fe77ccaeb468f77bd967b4f/2018/04/file_example_OGG_1920_13_3mg.ogg"
+    },
+    # ----------------------------------------------------------------------------------------------------------########
+    # ----------------------------------------------------------------------------------------------------------# WMV  #
+    # ----------------------------------------------------------------------------------------------------------########
     {
         "size": "1.2MB",
         "format": "wmv",
         "resolution": "640x480",
-        "url": "https://file-examples.com/wp-content/storage/2018/04/file_example_WMV_480_1_2MB.wmv"},
+        "url": "https://file-examples.com/wp-content/storage/2018/04/file_example_WMV_480_1_2MB.wmv"
+    },
     {
         "size": "1.6MB",
         "format": "wmv",
         "resolution": "640x480",
-        "url": "https://file-examples.com/wp-content/storage/2018/04/file_example_WMV_640_1_6MB.wmv"},
+        "url": "https://file-examples.com/wp-content/storage/2018/04/file_example_WMV_640_1_6MB.wmv"
+    },
     {
         "size": "4.9MB",
         "format": "wmv",
         "resolution": "1280x720",
-        "url": "https://file-examples.com/wp-content/storage/2018/04/file_example_WMV_1280_4_9MB.wmv"},
+        "url": "https://file-examples.com/wp-content/storage/2018/04/file_example_WMV_1280_4_9MB.wmv"
+    },
     {
         "size": "9.3MB",
         "format": "wmv",
         "resolution": "1920x1080",
-        "url": "https://file-examples.com/wp-content/storage/2018/04/file_example_WMV_1920_9_3MB.wmv"},
-    # WEBM
+        "url": "https://file-examples.com/wp-content/storage/2018/04/file_example_WMV_1920_9_3MB.wmv"
+    },
+    # ----------------------------------------------------------------------------------------------------------########
+    # ----------------------------------------------------------------------------------------------------------# WEBM #
+    # ----------------------------------------------------------------------------------------------------------########
     {
         "size": "900KB",
         "format": "webm",
         "resolution": "640x480",
-        "url": "https://file-examples.com/storage/fe77ccaeb468f77bd967b4f/2020/03/file_example_WEBM_480_900KB.webm"},
+        "url": "https://file-examples.com/storage/fe77ccaeb468f77bd967b4f/2020/03/file_example_WEBM_480_900KB.webm"
+    },
     {
         "size": "1.4MB",
         "format": "webm",
         "resolution": "640x360",
-        "url": "https://file-examples.com/storage/fe77ccaeb468f77bd967b4f/2020/03/file_example_WEBM_640_1_4MB.webm"},
+        "url": "https://file-examples.com/storage/fe77ccaeb468f77bd967b4f/2020/03/file_example_WEBM_640_1_4MB.webm"
+    },
     {
         "size": "3.6MB",
         "format": "webm",
         "resolution": "1280x720",
-        "url": "https://file-examples.com/storage/fe77ccaeb468f77bd967b4f/2020/03/file_example_WEBM_1280_3_6MB.webm"},
+        "url": "https://file-examples.com/storage/fe77ccaeb468f77bd967b4f/2020/03/file_example_WEBM_1280_3_6MB.webm"
+    },
     {
         "size": "3.7MB",
         "format": "webm",
         "resolution": "1920x1080",
-        "url": "https://file-examples.com/storage/fe77ccaeb468f77bd967b4f/2020/03/file_example_WEBM_1920_3_7MB.webm"},
+        "url": "https://file-examples.com/storage/fe77ccaeb468f77bd967b4f/2020/03/file_example_WEBM_1920_3_7MB.webm"
+    },
 ]
