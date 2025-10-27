e8c5819f789c4ff9087fff24d4d18dad2e0c663b
Felix Rizzolli <felix.rizzolli@outlook.de>
2025-10-27 15:49:08 +0000

    tests: bugfix in test_strip_tvdb_suffix

--- tests/unittests/shared/test_strip_tvdb_suffix.py
+++ tests/unittests/shared/test_strip_tvdb_suffix.py
@@ -8,7 +8,7 @@ from plex_leon.shared import strip_tvdb_suffix
     [
         ("Code Geass {tvdb-123}", "Code Geass"),
         ("Show {tvdb-1} Extra {tvdb-2}", "ShowExtra"),
-    ],0205
+    ],
 )
 def test_strip_tvdb_suffix(name: str, expected: str) -> None:
     assert strip_tvdb_suffix(name) == expected
