0fe92526228c0f99c78b5acb352b9e0938a33ec9
Felix Rizzolli <felix.rizzolli@outlook.de>
2026-03-01 16:20:03 +0000

    feat: add dialog components, add libraries table and add library dialog

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -6,38 +6,95 @@
         <CardDescription>Manage your API tokens</CardDescription>
       </CardHeader>
       <CardContent>
-        <form @submit.prevent="save">
-          <div class="grid w-full items-center gap-4">
-            <div class="flex flex-col space-y-1.5">
-              <Label for="tvdb">TVDB API Token</Label>
-              <Input id="tvdb" v-model="tvdb" />
-            </div>
-            <div class="flex flex-col space-y-1.5">
-              <Label for="tmdb">TMDB API Token</Label>
-              <Input id="tmdb" v-model="tmdb" />
-            </div>
+        <div class="grid w-full items-center gap-4">
+          <div class="flex flex-col space-y-1.5">
+            <Label for="tvdb">TVDB API Token</Label>
+            <Input id="tvdb" v-model="tvdb" />
           </div>
-          <CardFooter>
-            <div class="flex justify-end w-full">
-              <Button type="submit">Save</Button>
-            </div>
-          </CardFooter>
-        </form>
+          <div class="flex flex-col space-y-1.5">
+            <Label for="tmdb">TMDB API Token</Label>
+            <Input id="tmdb" v-model="tmdb" />
+          </div>
+        </div>
       </CardContent>
+      <CardFooter>
+        <form @submit.prevent="save">
+          <div class="flex justify-end w-full">
+            <Button type="submit">Save</Button>
+          </div>
+        </form>
+      </CardFooter>
     </Card>
+
     <Card>
       <CardHeader>
         <CardTitle>Media Libraries</CardTitle>
         <CardDescription>Manage your media libraries</CardDescription>
       </CardHeader>
       <CardContent>
-        <!-- TODO: implement media libraries management -->
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead> ID </TableHead>
+              <TableHead> Name </TableHead>
+              <TableHead> Type </TableHead>
+              <TableHead> Path </TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow v-for="lib in libraries" :key="lib.id">
+              <TableCell class="font-medium">{{ lib.id }}</TableCell>
+              <TableCell>{{ lib.name }}</TableCell>
+              <TableCell>{{ lib.type }}</TableCell>
+              <TableCell>{{ lib.path }}</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
       </CardContent>
+      <CardFooter>
+        <Dialog>
+          <form>
+            <DialogTrigger as-child>
+              <Button variant="outline"> Open Dialog </Button>
+            </DialogTrigger>
+            <DialogContent class="sm:max-w-106.25">
+              <DialogHeader>
+                <DialogTitle>Add Media Library</DialogTitle>
+                <DialogDescription>
+                  Add a new media library here. Click save when you're done.
+                </DialogDescription>
+              </DialogHeader>
+              <div class="grid gap-4">
+                <div class="grid gap-3">
+                  <Label for="name">Name</Label>
+                  <Input id="name" name="name" default-value="" />
+                </div>
+                <div class="grid gap-3">
+                  <Label for="type">Type</Label>
+                  <Input id="type" name="type" default-value="" />
+                </div>
+                <div class="grid gap-3">
+                  <Label for="path">Path</Label>
+                  <Input id="path" name="path" default-value="" />
+                </div>
+              </div>
+              <DialogFooter>
+                <DialogClose as-child>
+                  <Button variant="outline"> Cancel </Button>
+                </DialogClose>
+                <Button type="submit"> Save changes </Button>
+              </DialogFooter>
+            </DialogContent>
+          </form>
+        </Dialog>
+      </CardFooter>
     </Card>
   </div>
 </template>
 
 <script setup lang="ts">
+  import { ref, onMounted } from 'vue';
+
   import {
     Card,
     CardContent,
@@ -46,17 +103,44 @@
     CardHeader,
     CardTitle,
   } from '@/components/ui/card';
+  import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableFooter,
+    TableHead,
+    TableHeader,
+    TableRow,
+  } from '@/components/ui/table';
+  import {
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+  } from '@/components/ui/dialog';
   import { Input } from '@/components/ui/input';
   import { Label } from '@/components/ui/label';
+  import { Button } from '@/components/ui/button';
 
   import { useI18n } from 'vue-i18n';
   const { t } = useI18n();
-  
-  import { ref, onMounted } from 'vue';
-  import { Button } from '@/components/ui/button';
 
   const tvdb = ref<string>('');
   const tmdb = ref<string>('');
+
+  type Library = {
+    id: string;
+    name: string;
+    type: string;
+    path: string;
+  };
+
+  const libraries = ref<Array<Library>>([]);
 
   async function loadSettings() {
     try {
